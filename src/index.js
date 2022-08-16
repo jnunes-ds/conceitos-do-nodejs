13f5c16484f8520da28db7d17f9c01a4793b7eaa
Júnior Nunes <denisonprobable13@gmail.com>
2022-08-16 19:25:13 -0300

    feat: checksExistsUserAccount function created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,33 +8,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+
+  const USER = users.find((user) => user.username === username);
+
+  if (USER) return response.status(400).json({ error: "User already exists" });
+
+  request.user = USER;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
